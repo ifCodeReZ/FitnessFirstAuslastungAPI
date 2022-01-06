be461ea439ba20d45887b2134a79dec5aa63c68e
ifCodeReZ <niklas.brecht@gmx.net>
2022-01-06 19:42:30 +0100

    All bugs fixed!

--- ApiRequest.py
+++ ApiRequest.py
@@ -3,36 +3,37 @@ import requests
 from datetime import datetime
 
 
-#CREATE DATABASE
+# Create Database
 con = sqlite3.connect('database.db')
 cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS fitnessFirstAuslastung (Zeit text, Gendarmenmarkt double, PrenzlauerBerg double, Steglitz double, Wilmersdorf double, Zehlendorf double)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS fitnessFirstAuslastung (
+Zeit text, Gendarmenmarkt double, PrenzlauerBerg double, Steglitz double, Wilmersdorf double, Zehlendorf double)''')
 
-#API-Request @param clubNumber, @return utilization
-def auslastung(clubNumber):
-    url = f"https://www.fitnessfirst.de/club/api/checkins/{clubNumber}"
+
+# API-Request @param clubNumber, @return utilization
+def auslastung(clubnumber):
+    url = f"https://www.fitnessfirst.de/club/api/checkins/{clubnumber}"
     data = requests.get(url).json()
     checkins = data['data']['check_ins']
     allowedPeople = data['data']['allowed_people']
-    auslastung = round((checkins / allowedPeople) * 100, 1)
-    return auslastung
+    utilization = round((checkins / allowedPeople) * 100, 1)
+    return utilization
+
 
 if __name__ == "__main__":
-    #GET UTILIZATION
-    auslastungGedarmenmarkt = auslastung(clubNumber="berlin12")
-    auslastungPrenzlauerBerg = auslastung(clubNumber="berlin2")
-    auslastungSteglitz = auslastung(clubNumber="berlin3")
-    auslastungWilmersdorf = auslastung(clubNumber="berlin10")
-    auslastungZehlendorf = auslastung(clubNumber="berlin11")
-    #GET TIME AND DATE
+    # Get Utilization
+    auslastungGedarmenmarkt = auslastung(clubnumber="berlin12")
+    auslastungPrenzlauerBerg = auslastung(clubnumber="berlin2")
+    auslastungSteglitz = auslastung(clubnumber="berlin3")
+    auslastungWilmersdorf = auslastung(clubnumber="berlin10")
+    auslastungZehlendorf = auslastung(clubnumber="berlin11")
+    # Get time and date
     datumZeit = datetime.today()
-    # COPY DATA TO DATABASE
-    cur.execute(f'''INSERT INTO fitnessFirstAuslastung VALUES ('{datumZeit}','{auslastungGedarmenmarkt}','{auslastungPrenzlauerBerg}','{auslastungSteglitz}','{auslastungWilmersdorf}','{auslastungZehlendorf}')''')
+    # Copy data to database
+    cur.execute(f'''INSERT INTO fitnessFirstAuslastung VALUES ('{datumZeit}',
+    '{auslastungGedarmenmarkt}',
+    '{auslastungPrenzlauerBerg}',
+    '{auslastungSteglitz}',
+    '{auslastungWilmersdorf}',
+    '{auslastungZehlendorf}')''')
     con.commit()
-
-
-# Debug
-# print(auslastung)
-# print(datumZeit)
-# print(checkins)
-# print(allowedPeople)
