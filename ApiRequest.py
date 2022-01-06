00bbc6a37c22ccc82a4e17897288d65d70d664d3
ifCodeReZ <niklas.brecht@gmx.net>
2022-01-06 19:08:04 +0100

    Datacollection upgrade

--- ApiRequest.py
+++ ApiRequest.py
@@ -2,32 +2,36 @@ import sqlite3
 import requests
 from datetime import datetime
 
-#CONST
-clubNumber = "berlin11"
-checkins = 0
-allowedPeople = 0
 
-#SQL-DB erzeugen
-con = sqlite3.connect('daten.db')
+#CREATE DATABASE
+con = sqlite3.connect('database.db')
 cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS fitnessFirstAuslastung (Zeit text, Prozent double)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS fitnessFirstAuslastung (Zeit text, Gendarmenmarkt double, PrenzlauerBerg double, Steglitz double, Wilmersdorf double, Zehlendorf double)''')
+
+#API-Request @param clubNumber, @return utilization
+def auslastung(clubNumber):
+    url = f"https://www.fitnessfirst.de/club/api/checkins/{clubNumber}"
+    data = requests.get(url).json()
+    checkins = data['data']['check_ins']
+    allowedPeople = data['data']['allowed_people']
+    auslastung = round((checkins / allowedPeople) * 100, 1)
+    return auslastung
+
+if __name__ == "__main__":
+    auslastungGedarmenmarkt = auslastung(clubNumber="berlin12")
+    auslastungPrenzlauerBerg = auslastung(clubNumber="berlin2")
+    auslastungSteglitz = auslastung(clubNumber="berlin3")
+    auslastungWilmersdorf = auslastung(clubNumber="berlin10")
+    auslastungZehlendorf = auslastung(clubNumber="berlin11")
+    #GET TIME AND DATE
+    datumZeit = datetime.today()
+    # COPY DATA TO DATABASE
+    cur.execute(f'''INSERT INTO fitnessFirstAuslastung VALUES ('{datumZeit}','{auslastungGedarmenmarkt}','{auslastungPrenzlauerBerg}','{auslastungSteglitz}','{auslastungWilmersdorf}','{auslastungZehlendorf}')''')
+    con.commit()
 
 
-
-# API-Request
-url = f"https://www.fitnessfirst.de/club/api/checkins/{clubNumber}"
-data = requests.get(url).json()
-datumZeit = datetime.today()
-checkins = data['data']['check_ins']
-allowedPeople = data['data']['allowed_people']
-auslastung = round((checkins / allowedPeople) * 100, 1)
-
-# Funktionstest
+# Debug
 # print(auslastung)
 # print(datumZeit)
 # print(checkins)
 # print(allowedPeople)
-
-#DATEN IN SQL-DB ÜBERTRAGEN
-cur.execute(f'''INSERT INTO fitnessFirstAuslastung VALUES ('{datumZeit}','{auslastung}')''')
-con.commit()
